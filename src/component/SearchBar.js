0135e61d510209091c24e0e05dc218155014d590
Anurag7275977 <arjunsharma705463@gmail.com>
2023-05-23 02:59:53 +0530

    search bar assignmnet completed

---
+++ src/component/SearchBar.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './SearchBar.css';
+
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleChange = async (event) => {
+    const { value } = event.target;
+    setSearchTerm(value);
+
+    try {
+      const response = await axios.get(
+        'http://cdn-api.co-vin.in/api/v2/admin/location/states'
+      );
+
+      const states = response.data.states;
+      const filteredResults = states.filter((state) =>
+        state.state_name.toLowerCase().includes(value.toLowerCase())
+      );
+      setSearchResults(filteredResults);
+    } catch (error) {
+      console.log('Error fetching states:', error);
+    }
+  };
+
+  return (
+    <div className="searchContainer">
+      <div className="searchInputWrapper">
+        
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={handleChange}
+          className="searchInput"
+        />
+      </div>
+      <ul className="resultsContainer">
+        {searchResults.map((state) => (
+          <li key={state.state_id}>{state.state_name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SearchBar;
